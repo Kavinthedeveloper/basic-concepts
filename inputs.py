07a46edbe70c62c944792beff73c34afcf20bfc8
Kavinthedeveloper <115207533+Kavinthedeveloper@users.noreply.github.com>
2022-10-14 09:27:11 +0400

    Add files via upload

---
+++ inputs.py
@@ -0,0 +1,19 @@
+'''
+aa =float(input("final exam %"))
+print(aa)
+bb=input("enter your name")
+print(bb)
+cc=input("what is your grade")
+print(cc)
+dd=input("what i your hoby")
+print(dd)
+'''
+a=int(input("enter a number"))
+
+if a%2==0:
+    print("even number")
+else:
+    print("odd numbers")
+    
+
+
